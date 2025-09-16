f11c45feb72fa2ae5ffadd02ac7eac8a43c9dd56
lepzulnag <gin.the.quin@gmail.com>
2025-09-16 15:56:18 +0200

    ⛏️ Do not start drizzle when building

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,15 +1,21 @@
-import { dev } from '$app/environment';
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
-import * as schema from './schema';
-import { env } from '$env/dynamic/private';
+import { building, dev } from "$app/environment";
+import { env } from "$env/dynamic/private";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema";
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-if (!dev && !env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+export let db: ReturnType<typeof drizzle>;
 
-const client = createClient({
-	url: env.DATABASE_URL,
-	authToken: env.DATABASE_AUTH_TOKEN
-});
+if (building) {
+	db = {} as any;
+} else {
+	if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+	if (!dev && !env.DATABASE_AUTH_TOKEN) throw new Error("DATABASE_AUTH_TOKEN is not set");
 
-export const db = drizzle(client, { schema });
+	const client = createClient({
+		url: env.DATABASE_URL,
+		authToken: env.DATABASE_AUTH_TOKEN,
+	});
+
+	db = drizzle(client, { schema });
+}
