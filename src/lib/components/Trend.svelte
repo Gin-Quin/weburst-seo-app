844a24f41c2936f17c2c990037b44f441ccd356f
lepzulnag <gin.the.quin@gmail.com>
2025-09-22 16:58:22 +0200

    ✨ Potentiel SEO really done

---
+++ src/lib/components/Trend.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { formatPercent } from "$lib/numbers/formatPercent";
+	import IconArrowDownRegular from "phosphor-icons-svelte/IconArrowDownRegular.svelte";
+	import IconArrowUpRegular from "phosphor-icons-svelte/IconArrowUpRegular.svelte";
+
+	let {
+		trend,
+		size = "sm",
+	}: { trend: number | null | undefined; size?: "xs" | "sm" } = $props();
+</script>
+
+{#if trend && trend >= 0.1}
+	<div class="badge badge-success">
+		<IconArrowUpRegular class="text-small" />
+		<span class={size === "xs" ? "text-xs" : "text-sm"}>
+			{formatPercent(trend)}
+		</span>
+	</div>
+{:else if trend && trend < -0.1}
+	<div class="badge badge-warning">
+		<IconArrowDownRegular class="text-small" />
+		<span class={size === "xs" ? "text-xs" : "text-sm"}>
+			{formatPercent(trend)}
+		</span>
+	</div>
+{/if}
