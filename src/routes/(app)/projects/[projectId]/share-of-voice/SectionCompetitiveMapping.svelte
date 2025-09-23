119c4734b67bffbe0e9fbba2149e0862d749e609
lepzulnag <gin.the.quin@gmail.com>
2025-09-23 21:21:21 +0200

    🚀 Prepare for launch

--- src/routes/(app)/projects/[projectId]/share-of-voice/SectionCompetitiveMapping.svelte
+++ src/routes/(app)/projects/[projectId]/share-of-voice/SectionCompetitiveMapping.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
-	import { defineContent } from "$lib/i18n/locale.svelte";
+	import { defineContent, locale } from "$lib/i18n/locale.svelte";
+	import type {
+		AggregatedKeywordAnalysis,
+		AggregatedKeywordAnalysisData,
+	} from "$lib/server/clickhouse/services/keywords";
+	import {
+		Axis,
+		Highlight,
+		Layer,
+		Points,
+		ScatterChart,
+		Tooltip,
+	} from "layerchart";
+	import * as Chart from "$lib/components/ui/chart/index.js";
+	import type { SvelteSet } from "svelte/reactivity";
+	import { formatPercent } from "$lib/numbers/formatPercent";
 
 	const content = defineContent({
 		en: {
@@ -13,10 +28,83 @@
 				"Analyse comparative des domaines par part de voix et volume de mots-clés.",
 		},
 	});
+
+	type ChartData = {
+		key: string;
+		data: Array<AggregatedKeywordAnalysisData & { shareOfVoice: number }>;
+	};
+
+	const chartColors: Array<string> = [
+		"var(--color-primary)",
+		"var(--color-info-magenta)",
+		"var(--color-error)",
+	];
+
+	let {
+		analysisResultsWithTrend,
+		visibleDomains,
+		client,
+	}: {
+		analysisResultsWithTrend: AggregatedKeywordAnalysis;
+		visibleDomains: SvelteSet<string>;
+		client: AggregatedKeywordAnalysisData;
+	} = $props();
+
+	const { data, totalVolume } = $derived(analysisResultsWithTrend);
+
+	const { chartConfig, chartData } = $derived(
+		getChartData({
+			data,
+		}),
+	);
+
+	function getChartData({
+		data,
+	}: {
+		data: Array<AggregatedKeywordAnalysisData>;
+	}): {
+		chartConfig: Chart.ChartConfig;
+		chartData: Array<ChartData>;
+	} {
+		let index = 0;
+
+		const chartData: Array<ChartData> = [
+			{
+				key: client.domain,
+				data: [
+					{
+						...client,
+						shareOfVoice: (client.volume / totalVolume) * 100,
+					},
+				],
+			},
+		];
+		const chartConfig: Chart.ChartConfig = {};
+
+		for (const item of data.slice(0, 10)) {
+			chartData.push({
+				key: item.domain,
+				data: item
+					? [
+							{
+								...item,
+								shareOfVoice: (item.volume / totalVolume) * 100,
+							},
+						]
+					: [],
+			});
+			chartConfig[item.domain] = {
+				label: item.domain,
+			};
+			index++;
+		}
+
+		return { chartConfig, chartData };
+	}
 </script>
 
-<div class="card">
-	<header>
+<div class="card col justify-stretch">
+	<header class="shrink-0">
 		<div class="title">
 			{$content.title}
 		</div>
@@ -25,5 +113,20 @@
 		</div>
 	</header>
 
-	<main>TODO</main>
+	<main class="col justify-stretch w-full grow">
+		<Chart.Container config={chartConfig} class="h-full">
+			<ScatterChart
+				x="topTenKeywordCount"
+				y="shareOfVoice"
+				r="topThreeKeywordCount"
+				xPadding={[20, 20]}
+				yPadding={[20, 20]}
+				series={chartData}
+				renderContext="svg"
+				grid={{
+					x: false,
+				}}
+			></ScatterChart>
+		</Chart.Container>
+	</main>
 </div>
